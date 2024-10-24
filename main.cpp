2fa3533a3d44a80b9f0a71469773a29d526b0e41
MaksimSuslyakov227 <mkostin531@gmail.com>
2024-10-24 22:24:07 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Введите первое число : "; #Приглашение первого числа
+    cin >> a;
+    cout << "Введите второе число : "; #Приглашение второго числа
+    cin >> b;
+    int sum = a + b;
+    cout << "Sum : " << sum << endl;
+}
